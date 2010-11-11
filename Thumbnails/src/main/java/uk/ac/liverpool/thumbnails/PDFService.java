7ef9dfbda56f1b88ee416a444981f56309ad7eb3
corubolo <corubolo@aaf4d3da-0c10-11df-8c7c-8529219665b1>
2010-11-11 08:59:45 +0000

    Added Copyright info

--- Thumbnails/src/main/java/uk/ac/liverpool/thumbnails/PDFService.java
+++ Thumbnails/src/main/java/uk/ac/liverpool/thumbnails/PDFService.java
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ * This Library is :
+ * 
+ *     Copyright © 2010 Fabio Corubolo - all rights reserved
+ *     corubolo@gmail.com
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * see COPYING.LESSER.txt
+ * 
+ ******************************************************************************/
 package uk.ac.liverpool.thumbnails;
 
 import java.awt.Color;
