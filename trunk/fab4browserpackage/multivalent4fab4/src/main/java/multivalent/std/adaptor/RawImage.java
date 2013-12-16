cbef967634e5fba552047a65ba1f34abff6d81dd
corubolo <corubolo@aaf4d3da-0c10-11df-8c7c-8529219665b1>
2013-12-16 16:05:56 +0000

    fix build problems

--- trunk/fab4browserpackage/multivalent4fab4/src/main/java/multivalent/std/adaptor/RawImage.java
+++ trunk/fab4browserpackage/multivalent4fab4/src/main/java/multivalent/std/adaptor/RawImage.java
@@ -156,111 +156,111 @@ public class RawImage extends MediaAdaptor {
         }
 
         File f = MediaLoader.FileCache.get(doc.getURI());
-        if (f != null) {
-            try {
-                Metadata metadata = JpegMetadataReader.readMetadata(f);
-                StringBuilder sb = new StringBuilder(5000);
+        // if (f != null) {
+        //     try {
+        //         Metadata metadata = JpegMetadataReader.readMetadata(f);
+        //         StringBuilder sb = new StringBuilder(5000);
 
-                Directory exifDirectory = metadata.getDirectory(
-                        ExifDirectory.class);
-                Iterator tags = exifDirectory.getTagIterator();
-                if (tags.hasNext())
-                    sb.append("EXIF information:\n");
-                List<String> ot = new LinkedList<String>();
-                while (tags.hasNext()) {
-                    Tag tag = (Tag) tags.next();
-                    String c;
-                    switch (tag.getTagType()) {
-                    case ExifDirectory.TAG_DATETIME_ORIGINAL:
-                        c = "Date taken";
-                        break;
+        //         Directory exifDirectory = metadata.getDirectory(
+        //                 ExifDirectory.class);
+        //         Iterator tags = exifDirectory.getTagIterator();
+        //         if (tags.hasNext())
+        //             sb.append("EXIF information:\n");
+        //         List<String> ot = new LinkedList<String>();
+        //         while (tags.hasNext()) {
+        //             Tag tag = (Tag) tags.next();
+        //             String c;
+        //             switch (tag.getTagType()) {
+        //             case ExifDirectory.TAG_DATETIME_ORIGINAL:
+        //                 c = "Date taken";
+        //                 break;
 
-                    case ExifDirectory.TAG_COPYRIGHT:
-                        c = "Copyright";
-                        break;
-                    case ExifDirectory.TAG_IMAGE_DESCRIPTION:
-                        c = "Description";
-                        break;
-                    default:
-                        c = null;
-                        ot.add(tag.toString());
-                        break;
-                    }
-                    if (c != null) {
-                        sb.append(c);
-                        sb.append(": ");
-                        sb.append(tag.getDescription());
-                        sb.append('\n');
-                    }
-                }
+        //             case ExifDirectory.TAG_COPYRIGHT:
+        //                 c = "Copyright";
+        //                 break;
+        //             case ExifDirectory.TAG_IMAGE_DESCRIPTION:
+        //                 c = "Description";
+        //                 break;
+        //             default:
+        //                 c = null;
+        //                 ot.add(tag.toString());
+        //                 break;
+        //             }
+        //             if (c != null) {
+        //                 sb.append(c);
+        //                 sb.append(": ");
+        //                 sb.append(tag.getDescription());
+        //                 sb.append('\n');
+        //             }
+        //         }
 
-                Directory iptcDir = metadata.getDirectory(IptcDirectory.class);
-                tags = iptcDir.getTagIterator();
-                if (tags.hasNext())
-                    sb.append("\nIPTC information:\n");
-                while (tags.hasNext()) {
-                    Tag tag = (Tag) tags.next();
-                    String c;
-                    switch (tag.getTagType()) {
-                    case IptcDirectory.TAG_COPYRIGHT_NOTICE:
-                        c = "Copyright";
-                        break;
-                    case IptcDirectory.TAG_CAPTION:
-                        c = "Caption";
-                        break;
-                    case IptcDirectory.TAG_BY_LINE:
-                        c = "Author";
-                        break;
-                    case IptcDirectory.TAG_DATE_CREATED:
-                        c = "Date";
-                        break;
-                    case IptcDirectory.TAG_TIME_CREATED:
-                        c = "Time";
-                        break;
-                    default:
-                        c = null;
-                        ot.add(tag.toString());
-                        break;
-                    }
-                    if (c != null) {
-                        sb.append(c);
-                        sb.append(": ");
-                        sb.append(tag.getDescription());
-                        sb.append('\n');
-                    }
-                }
-                sb.append("\nOther information:\n");
-                for (String tt : ot) {
-                    sb.append(tt);
-                    sb.append('\n');
-                }
+        //         Directory iptcDir = metadata.getDirectory(IptcDirectory.class);
+        //         tags = iptcDir.getTagIterator();
+        //         if (tags.hasNext())
+        //             sb.append("\nIPTC information:\n");
+        //         while (tags.hasNext()) {
+        //             Tag tag = (Tag) tags.next();
+        //             String c;
+        //             switch (tag.getTagType()) {
+        //             case IptcDirectory.TAG_COPYRIGHT_NOTICE:
+        //                 c = "Copyright";
+        //                 break;
+        //             case IptcDirectory.TAG_CAPTION:
+        //                 c = "Caption";
+        //                 break;
+        //             case IptcDirectory.TAG_BY_LINE:
+        //                 c = "Author";
+        //                 break;
+        //             case IptcDirectory.TAG_DATE_CREATED:
+        //                 c = "Date";
+        //                 break;
+        //             case IptcDirectory.TAG_TIME_CREATED:
+        //                 c = "Time";
+        //                 break;
+        //             default:
+        //                 c = null;
+        //                 ot.add(tag.toString());
+        //                 break;
+        //             }
+        //             if (c != null) {
+        //                 sb.append(c);
+        //                 sb.append(": ");
+        //                 sb.append(tag.getDescription());
+        //                 sb.append('\n');
+        //             }
+        //         }
+        //         sb.append("\nOther information:\n");
+        //         for (String tt : ot) {
+        //             sb.append(tt);
+        //             sb.append('\n');
+        //         }
 
-                Iterator directories = metadata.getDirectoryIterator();
-                while (directories.hasNext()) {
-                    Directory directory = (Directory) directories.next();
-                    tags = directory.getTagIterator();
-                    while (tags.hasNext()) {
-                        Tag tag = (Tag) tags.next();
-                        System.out.println(tag);
-                    }
-                }
-                Layer sc = doc.getLayer(Layer.SCRATCH);
-                Map<String, Object> m = new HashMap<String, Object>();
-                m.put("text", sb.toString());
-                ImageInternalDataFrame ls =
-                        (ImageInternalDataFrame) Behavior.getInstance(
-                                "ImageInternalDataFrame",
-                                "uk.ac.liverpool.fab4.ImageInternalDataFrame",
-                                null, m, sc);
-                ls.setTitle("Image Metadata");
-                ls.setTransparent(true);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            Layer ll = doc.getLayer(Layer.PERSONAL);
-            if (ll != null)
-                ll.destroy();
-        }
+        //         Iterator directories = metadata.getDirectoryIterator();
+        //         while (directories.hasNext()) {
+        //             Directory directory = (Directory) directories.next();
+        //             tags = directory.getTagIterator();
+        //             while (tags.hasNext()) {
+        //                 Tag tag = (Tag) tags.next();
+        //                 System.out.println(tag);
+        //             }
+        //         }
+        //         Layer sc = doc.getLayer(Layer.SCRATCH);
+        //         Map<String, Object> m = new HashMap<String, Object>();
+        //         m.put("text", sb.toString());
+        //         ImageInternalDataFrame ls =
+        //                 (ImageInternalDataFrame) Behavior.getInstance(
+        //                         "ImageInternalDataFrame",
+        //                         "uk.ac.liverpool.fab4.ImageInternalDataFrame",
+        //                         null, m, sc);
+        //         ls.setTitle("Image Metadata");
+        //         ls.setTransparent(true);
+        //     } catch (Exception e) {
+        //         e.printStackTrace();
+        //     }
+        //     Layer ll = doc.getLayer(Layer.PERSONAL);
+        //     if (ll != null)
+        //         ll.destroy();
+        // }
         return l; // constructed image
     }
 
